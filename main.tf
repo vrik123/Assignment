c3b94de5c5c2deb5b8ae67b657163becd83fb0a3
vrik123 <42827942+vrik123@users.noreply.github.com>
2021-02-11 15:23:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,24 @@ provider "aws" {
   access_key = ""
   secret_key = ""
 }
-module "VPC"{
-   source            =  ".//Modules/VPC"
-  }
-module "security"{
-   source            =  ".//Modules/security"	
-   vpc-id            ="${module.VPC.vpc-id}"
+module "VPC" {
+  source = ".//Modules/VPC"
+  availability_zone1 = "${var.availability_zone1}"
+  availability_zone2 = "${var.availability_zone2}"
+  main_vpc_cidr      = "${var.main_vpc_cidr}"
 }
-module "ASG"{
-   source            =  ".//Modules/ASG"	
-   instance-sg       = "${module.security.instance-sg}"
-   loadbalancer-sg   = "${module.security.loadbalancer-sg}"
-   sub1              = "${module.VPC.sub1}"
-   sub2              = "${module.VPC.sub2}"
-   pub1              = "${module.VPC.pub1}"
-   pub2              = "${module.VPC.pub2}"
-
+module "security" {
+  source = ".//Modules/security"
+  vpc-id = "${module.VPC.vpc-id}"
+}
+module "ASG" {
+  source          = ".//Modules/ASG"
+  instance-sg     = "${module.security.instance-sg}"
+  loadbalancer-sg = "${module.security.loadbalancer-sg}"
+  sub1            = "${module.VPC.sub1}"
+  sub2            = "${module.VPC.sub2}"
+  pub1            = "${module.VPC.pub1}"
+  pub2            = "${module.VPC.pub2}"
+  key_name        =  "${var.key_name}"
+  AMIS            =  "${var.AMIS}"
 }
